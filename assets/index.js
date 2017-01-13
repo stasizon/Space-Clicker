b0b3d5d235153c4993ef92a309dc4bba0a9bcd1f
stasizon <stasizon@gmail.com>
2017-01-13 08:58:24 +0200

    Finish right panel

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,12 @@
-//=require blocks/leftPanel/leftPanel.js 
-//=require blocks/comanderSign/comanderSign.js 
-//=require blocks/counter/counter.js 
-//=require blocks/button/button.js 
-//=require blocks/screen/screen.js 
-//=require blocks/note/note.js 
-//=require blocks/rightPanel/rightPanel.js 
-//=require blocks/amountInfo/amountInfo.js 
+window.onload = function() {
+
+    //=require blocks/leftPanel/leftPanel.js
+    //=require blocks/comanderSign/comanderSign.js
+    //=require blocks/counter/counter.js
+    //=require blocks/button/button.js 
+    //=require blocks/screen/screen.js
+    //=require blocks/note/note.js
+    //=require blocks/rightPanel/rightPanel.js
+    //=require blocks/amountInfo/amountInfo.js
+
+}

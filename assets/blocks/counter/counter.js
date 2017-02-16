f63e0d7125d7c8a4f0e1cba41009f63737645d4a
stasizon <stasizon@gmail.com>
2017-02-16 11:03:38 +0200

    Redesign

--- assets/blocks/counter/counter.js
+++ assets/blocks/counter/counter.js
@@ -1,13 +1,20 @@
-// var counterDisplays = [
-//     document.getElementsByClassName('counter__text')[0].innerHTML,
-//     document.getElementsByClassName('counter__text')[1].innerHTML,
-//     document.getElementsByClassName('counter__text')[2].innerHTML
-// ];
-
 var score = 0;
+var distance = 100;
 
-function addScore(value) {
+var distanceDisplay = document.getElementsByClassName('target__distance')[0];
+
+function calculateScore(value) {
     score = score + value;
     document.getElementsByClassName('counter__text')[0].innerHTML = score;
+    calculateDistance(value);
+    checkUpgrade();
+}
+
+function calculateDistance(value) {
+    distance = distance - value;
+    distanceDisplay.innerHTML = '[' + distance + ']';
+}
+
+function checkUpgrade() {
 
 }
--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,83 @@
+'use strict'
+// VIEW
+
+var view = {
+
+    buttonClick: function(state) {
+
+        if (state) {
+            document.getElementById('button').classList.add('button_active');
+        } else {
+            document.getElementById('button').classList.remove('button_active');
+        }
+
+    },
+
+    showScore: function(n) {
+        var score = document.getElementById('counterScore');
+        score.innerHTML = n;
+    }
+
+}
+
+// MODEL
+
+var model = {
+
+    score: 0,
+
+    addScore: function(score) {
+        this.score++;
+
+        view.showScore(this.score);
+    }
+
+}
+
+//CONTROLLER
+
+var controller = {
+
+    buttonClick: function(buttonState) {
+
+        if (buttonState) {
+            model.addScore(1);
+        }
+
+        view.buttonClick(buttonState);
+    }
+
+}
+
+// INIT FUNCTION
+
 window.onload = function() {
 
-    //=require blocks/counter/counter.js
-    //=require blocks/button/button.js
-    //=require blocks/leftPanel/leftPanel.js
-    //=require blocks/comanderSign/comanderSign.js
-    //=require blocks/screen/screen.js
-    //=require blocks/note/note.js
-    //=require blocks/rightPanel/rightPanel.js
-    //=require blocks/amountInfo/amountInfo.js
+    var app = {
+
+        init: function() {
+            this.main();
+            this.event();
+        },
+
+        main: function() {
+
+        },
+
+        event: function() {
+
+            document.getElementById('button').onmousedown = function() {
+                controller.buttonClick(true);
+            }
+
+            document.getElementById('button').onmouseup = function() {
+                controller.buttonClick(false);
+            }
+
+        }
+
+    };
+
+    app.init();
 
 }
